6339c45178fcb320b64716029a32bd1e2c025dc4
Guido <guidompr@gmail.com>
2021-04-30 20:33:39 -0300

    ABM

--- TPs3/src/BinarySearchTree/ABM.java
+++ TPs3/src/BinarySearchTree/ABM.java
@@ -6,29 +6,27 @@ public class ABM<T> {
     public ABM(){}
 
     public void addOrganism(Organism organism, BinarySearchTree<T> tree){
-        if(!tree.exists((Comparable<T>) organism)){
             tree.insert((Comparable<T>) organism);
-        }
-        else{
-            System.out.println("This organism is already in the system.");
-        }
     }
 
     public void eliminateOrganism(Organism organism, BinarySearchTree<T> tree){
-        if(tree.exists((Comparable<T>) organism)){
             tree.delete((Comparable<T>) organism);
-        }
-        else {
-            System.out.println("This organism is not in the system.");
-        }
     }
 
-    public void editOrganism(Organism organism, BinarySearchTree<T> tree){
-        if(tree.exists((Comparable<T>) organism)){
-            String newDesc = Scanner.getString("Inserte la nueva descripcion: ");
+    public void editOrganism(Organism organism, BinarySearchTree<T> tree, String newDesc){
                 tree.delete((Comparable<T>) organism);
                 organism.setDescription(newDesc);
                 tree.insert((Comparable<T>) organism);
+    }
+
+    public void printReport(BinarySearchTree<T> tree){
+        if (!tree.isEmpty()){
+            printReport(tree.getLeft());
+            System.out.println("Code: " + ((Organism) tree.getRoot()).getCode() +
+                                "\nType: " + ((Organism) tree.getRoot()).getType()+"\n");
+            printReport(tree.getRight());
         }
     }
+
 }
+
