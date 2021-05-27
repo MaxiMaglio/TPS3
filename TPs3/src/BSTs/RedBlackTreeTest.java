4d6735fb8417defb375958f0b36c0a0a22b9f1cd
Juan Manuel PM <juanmanuel.perezmenendez@ing.austral.edu.ar>
2021-05-27 18:49:37 -0300

    BSTs

---
+++ TPs3/src/BSTs/RedBlackTreeTest.java
@@ -0,0 +1,20 @@
+package BSTs;
+
+import org.junit.jupiter.api.Test;
+
+class RedBlackTreeTest {
+
+    @Test
+    void preorder() {
+        RedBlackTree<Integer> h = new RedBlackTree<>();
+        h.insert(10);
+        h.insert(20);
+        h.insert(5);
+        h.insert(9);
+        h.insert(4);
+        h.insert(30);
+        h.insert(55);
+        h.insert(54);
+        h.preorder();
+    }
+}
